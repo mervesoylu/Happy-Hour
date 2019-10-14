4bc105fb09bd9fb5f443ec602482a5200d254efc
Dyfust <47803829+Dyfust@users.noreply.github.com>
2019-10-14 14:03:24 +1100

    Implemented bar & barrels.

---
+++ Happy Hour - Alpha/Assets/_Project/Bars/Bar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    #region --------------------------dependencies
+    [SerializeField] BarrelController _barrelPrefab;
+    #endregion
+
+    #region --------------------------interfaces
+    public void SpawnBarrel()
+    {
+        int hatch = Random.Range(0, _barHatches);
+        float unitCircle = (360f / _barHatches * hatch) * Mathf.Deg2Rad;
+        _currentDirection = new Vector3(Mathf.Cos(unitCircle), 0f, Mathf.Sin(unitCircle));
+        _initialRotation = _transform.rotation;
+        transform.rotation = Quaternion.LookRotation(Vector3.Cross(_currentDirection, Vector3.up), Vector3.up);
+        Vector3 spawnPosition = _barOrigin.position + _barRadius * _currentDirection;
+
+        Instantiate(_barrelPrefab, spawnPosition, Quaternion.identity).Roll(_currentDirection);
+    }
+    #endregion
+
+    #region --------------------------unity messages
+    private void Awake()
+    {
+        _transform = transform;
+    }
+    #endregion
+
+    #region --------------------------details
+    [SerializeField] Transform _barOrigin;
+    [SerializeField] float _barRadius;
+    [SerializeField] int _barHatches;
+    Vector3 _currentDirection;
+    Quaternion _initialRotation;
+
+    Transform _transform;
+    #endregion
+}
---
+++ Happy Hour - Alpha/Assets/_Project/World Objects/Barrels/BarrelController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelController : MonoBehaviour
+{
+    #region ----------------------------dependencies
+    Rigidbody _rigidbody;
+    #endregion
+
+    #region ----------------------------interfaces
+    public void Roll(Vector3 direction)
+    {
+        _rigidbody.AddForce(direction * _speed, ForceMode.VelocityChange);
+    }
+    #endregion
+
+    #region ----------------------------unity messages
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            other.GetComponent<Project.CharacterController>().TakeDamage();
+        }
+
+        Destroy(gameObject);
+    }
+    #endregion
+
+    #region ----------------------------details
+    [SerializeField] float _speed;
+
+    Transform _transform;
+    #endregion
+}
