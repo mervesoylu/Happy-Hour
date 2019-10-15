44ccb68f162d4d025d0b22ae58c01572d2b56253
Behnam Rasooli <ben.rasooli@gmail.com>
2019-10-15 15:14:26 +1100

    Add Pause Menu

--- Happy Hour - Alpha/Assets/_Project/MainMenu/MainMenuController.cs
+++ Happy Hour - Alpha/Assets/_Project/MainMenu/MainMenuController.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Button _playButton;
+    [SerializeField] Button _exitButton;
     void Start()
     {
-        
+        EventSystem.current.SetSelectedGameObject(_playButton.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        _playButton.onClick.AddListener(() => SceneManager.LoadScene(1));
+        _exitButton.onClick.AddListener(() => Application.Quit());
+    }
+
+    void OnDisable()
+    {
+        _playButton.onClick.RemoveAllListeners();
+        _exitButton.onClick.RemoveAllListeners();
     }
 }
--- Happy Hour - Alpha/Assets/_Project/PauseMenu/PauseMenuController.cs
+++ Happy Hour - Alpha/Assets/_Project/PauseMenu/PauseMenuController.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
 
 public class PauseMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Button _resumeButton;
+    [SerializeField] Button _mainMenuButton;
+    [SerializeField] Canvas _pauseMenuCanvas;
+    
     void Update()
     {
-        
+        if (XCI.GetButtonDown(XboxButton.Start))
+            pauseGame();
+    }
+
+    void pauseGame()
+    {
+        Time.timeScale = 0;
+        _pauseMenuCanvas.gameObject.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(_resumeButton.gameObject);
+    }
+
+    void resumeGame()
+    {
+        Time.timeScale = 1;
+        _pauseMenuCanvas.gameObject.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        _resumeButton.onClick.AddListener(resumeGame);
+        _mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene(0));
+        _pauseMenuCanvas.gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        _resumeButton.onClick.RemoveAllListeners();
+        _mainMenuButton.onClick.RemoveAllListeners();
     }
 }
