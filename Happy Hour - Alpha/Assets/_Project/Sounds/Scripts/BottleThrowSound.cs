decaf7f6fd4566f1a1a72231987edfb4549c476a
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2019-10-28 13:34:49 +1100

    added sounds into the sounds folder

---
+++ Happy Hour - Alpha/Assets/_Project/Sounds/Scripts/BottleThrowSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleThrowSound : MonoBehaviour
+{
+    public GameObject throwSound;
+    public GameObject breakSound;
+
+    void Start()
+    {
+        throwSound.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Character")
+        {
+            breakSound.SetActive(true);
+        }
+    }
+}
