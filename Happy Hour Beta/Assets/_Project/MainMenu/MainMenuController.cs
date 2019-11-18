ca117a6cb46335f04d4b9eed68cbfe569176087f
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2019-11-19 10:17:57 +1100

    added functioning credits and controls panels

--- Happy Hour Beta/Assets/_Project/MainMenu/MainMenuController.cs
+++ Happy Hour Beta/Assets/_Project/MainMenu/MainMenuController.cs
@@ -2,11 +2,19 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using XboxCtrlrInput;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] Button _playButton;
     [SerializeField] Button _exitButton;
+    [SerializeField] Button _controlsButton;
+    [SerializeField] Button _creditsButton;
+
+    [SerializeField] GameObject _mainMenuPanel;
+    [SerializeField] GameObject _controlsPanel;
+    [SerializeField] GameObject _creditsPanel;
+
     void Start()
     {
         EventSystem.current.SetSelectedGameObject(_playButton.gameObject);
@@ -16,11 +24,29 @@ public class MainMenuController : MonoBehaviour
     {
         _playButton.onClick.AddListener(() => SceneManager.LoadScene(1));
         _exitButton.onClick.AddListener(() => Application.Quit());
+        _controlsButton.onClick.AddListener(() => {
+            _mainMenuPanel.SetActive(false);
+            _controlsPanel.SetActive(true);
+        });
+        _creditsButton.onClick.AddListener(() => {
+            _mainMenuPanel.SetActive(false);
+            _creditsPanel.SetActive(true);
+        });
     }
 
     void OnDisable()
     {
         _playButton.onClick.RemoveAllListeners();
         _exitButton.onClick.RemoveAllListeners();
+    }
+
+    void Update()
+    {
+        if (XCI.GetButtonDown(XboxButton.B))
+        {
+            _mainMenuPanel.SetActive(true);
+            _controlsPanel.SetActive(false);
+            _creditsPanel.SetActive(false);
+        }
     }
 }
