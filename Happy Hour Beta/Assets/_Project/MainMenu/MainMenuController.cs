a9fc9e6b9ba3d4f932dc95f83d4b438e0a4f6bf6
Dyfust <47803829+Dyfust@users.noreply.github.com>
2019-11-25 14:47:46 +1100

    Fixed navigation bug in the main menu.

--- Happy Hour Beta/Assets/_Project/MainMenu/MainMenuController.cs
+++ Happy Hour Beta/Assets/_Project/MainMenu/MainMenuController.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using XboxCtrlrInput;
+using System.Collections.Generic;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -15,20 +16,31 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] GameObject _controlsPanel;
     [SerializeField] GameObject _creditsPanel;
 
+    [SerializeField] LinkedList<Button> _navigationPath = new LinkedList<Button>();
+    LinkedListNode<Button> _current;
+
     void Start()
     {
-        EventSystem.current.SetSelectedGameObject(_playButton.gameObject);
+        // Initialize the navigation path.
+        _navigationPath.AddLast(_exitButton);
+        _navigationPath.AddLast(_controlsButton);
+        _navigationPath.AddLast(_playButton);
+        _navigationPath.AddLast(_creditsButton);
+
+        _current = _navigationPath.Find(_playButton);
     }
 
     void OnEnable()
     {
         _playButton.onClick.AddListener(() => SceneManager.LoadScene(1));
         _exitButton.onClick.AddListener(() => Application.Quit());
-        _controlsButton.onClick.AddListener(() => {
+        _controlsButton.onClick.AddListener(() =>
+        {
             _mainMenuPanel.SetActive(false);
             _controlsPanel.SetActive(true);
         });
-        _creditsButton.onClick.AddListener(() => {
+        _creditsButton.onClick.AddListener(() =>
+        {
             _mainMenuPanel.SetActive(false);
             _creditsPanel.SetActive(true);
         });
@@ -44,12 +56,71 @@ public class MainMenuController : MonoBehaviour
     {
         for (int i = 1; i < 5; i++)
         {
-            if (XCI.GetButtonDown(XboxButton.B, (XboxController)i))
+            XboxController current = (XboxController)i;
+
+            if (XCI.GetButtonDown(XboxButton.A, current))
+            {
+                _current.Value.onClick.Invoke();
+            }
+
+            if (XCI.GetButtonDown(XboxButton.B, current))
             {
                 _mainMenuPanel.SetActive(true);
                 _controlsPanel.SetActive(false);
                 _creditsPanel.SetActive(false);
+                UpdateSelectedButton();
+            }
+
+            // DPad.
+            if (XCI.GetButtonDown(XboxButton.DPadRight, current))
+                Next();
+
+            if (XCI.GetButtonDown(XboxButton.DPadLeft, current))
+                Previous();
+
+            // Joystick.
+            if (XCI.GetAxisRaw(XboxAxis.LeftStickX, current) > 0f)
+            {
+                if (Time.time >= _nextTime)
+                {
+                    Next();
+                    _nextTime = Time.time + _interval;
+                }
+            }
+
+            if (XCI.GetAxisRaw(XboxAxis.LeftStickX, current) < 0f)
+            {
+                if (Time.time >= _nextTime)
+                {
+                    Previous();
+                    _nextTime = Time.time + _interval;
+                }
             }
         }
+    }
+    private static float _interval = 0.25f;
+    private float _nextTime;
+
+    private void Next()
+    {
+        if (_current.Next != null)
+        {
+            _current = _current.Next;
+            UpdateSelectedButton();
+        }
+    }
+
+    private void Previous()
+    {
+        if (_current.Previous != null)
+        {
+            _current = _current.Previous;
+            UpdateSelectedButton();
+        }
+    }
+
+    private void UpdateSelectedButton()
+    {
+        EventSystem.current.SetSelectedGameObject(_current.Value.gameObject);
     }
 }
